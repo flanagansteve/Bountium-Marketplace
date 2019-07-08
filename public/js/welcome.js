249c89d037bfd9f2a5d316fb5b565a2068cfba5e
Steve Flanagan <flanagan.s@husky.neu.edu>
2019-07-08 17:14:33 -0400

    button links->card links

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -12,8 +12,24 @@ window.addEventListener('load', async () => {
       React.createElement("p", {}, "The Bountium Marketplace uses smart contracts to make sure all jobs are processed fairly and automatically. To use these, you'll need to install ",
       React.createElement("a", {href : "https://metamask.io"}, "Metamask.")),
       React.createElement("p", {}, "To use the example marketplaces, set Metamask to the Ropsten test network."),
-      React.createElement("a", {className:"btn btn-primary", href:"./post"}, "Post an Order"),
-      React.createElement("a", {className:"btn btn-info float-right", href:"./work"}, "Fulfill Orders"),
+      React.createElement("div", {className:"row"},
+        React.createElement("a", {href:"./post/", className:"link-unstyled col-sm-6"},
+          React.createElement("div", {className:"card bg-primary text-white"},
+            React.createElement("div", {className:"card-body"},
+              React.createElement("h5", {className:"card-title"}, "Need to get something done?"),
+              React.createElement("p", {className:"card-text"}, "Post a job and let our global network of suppliers and workers do it right.")
+            )
+          )
+        ),
+        React.createElement("a", {href:"./work/", className:"link-unstyled col-sm-6"},
+          React.createElement("div", {className:"card bg-info text-white"},
+            React.createElement("div", {className:"card-body"},
+              React.createElement("h5", {className:"card-title"}, "Want to make some money?"),
+              React.createElement("p", {className:"card-text"}, "Access a world of fair and open revenue opportunities, across a variety of industries")
+            )
+          )
+        )
+      )
     ),
     document.getElementById("dashboard")
   );
